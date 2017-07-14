82d30b978855a992cb5fe327e983e6bd48aff147
Jordan Dhaenens <jordan.dhaenens@gmail.com>
2017-07-14 15:44:30 -0500

    finished exercise

--- Program.cs
+++ Program.cs
@@ -1,12 +1,180 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace dreamTeam
 {
+    public interface ITeammate
+    {
+        string Specialty { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        string FullName { get; }
+        void Work();
+    }
+
+    public class Matt : ITeammate
+    {
+        public string Specialty { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string FullName {
+            get { return FirstName + " " + LastName;}
+        }
+
+        // Constructor
+        public Matt (string spec, string first, string last)
+        {
+            this.Specialty = spec;
+            this.FirstName = first;
+            this.LastName = last;
+        }
+        public void Work()
+        {
+            Console.WriteLine($"la di da, I'm {FullName} and my speciality is {Specialty}");
+        }
+    }
+
+
+     public class Eliza : ITeammate
+    {
+        public string Specialty { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string FullName {
+            get { return FirstName + " " + LastName;}
+        }
+
+        // Constructor
+        public Eliza (string spec, string first, string last)
+        {
+            this.Specialty = spec;
+            this.FirstName = first;
+            this.LastName = last;
+        }
+        public void Work()
+        {
+            Console.WriteLine($"la di da, I'm {FullName} and my speciality is {Specialty}");
+        }
+    }
+
+    public class Andrew : ITeammate
+    {
+        public string Specialty { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string FullName {
+            get { return FirstName + " " + LastName;}
+        }
+
+        // Constructor
+        public Andrew (string spec, string first, string last)
+        {
+            this.Specialty = spec;
+            this.FirstName = first;
+            this.LastName = last;
+        }
+        public void Work()
+        {
+            Console.WriteLine($"la di da, I'm {FullName} and my speciality is {Specialty}");
+        }
+    }
+
+    public class Chaz : ITeammate
+    {
+        public string Specialty { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string FullName {
+            get { return FirstName + " " + LastName;}
+        }
+
+        // Constructor
+        public Chaz (string spec, string first, string last)
+        {
+            this.Specialty = spec;
+            this.FirstName = first;
+            this.LastName = last;
+        }
+        public void Work()
+        {
+            Console.WriteLine($"la di da, I'm {FullName} and my speciality is {Specialty}");
+        }
+    }
+
+    public class Tamela : ITeammate
+    {
+        public string Specialty { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string FullName {
+            get { return FirstName + " " + LastName;}
+        }
+
+        // Constructor
+        public Tamela (string spec, string first, string last)
+        {
+            this.Specialty = spec;
+            this.FirstName = first;
+            this.LastName = last;
+        }
+        public void Work()
+        {
+            Console.WriteLine($"la di da, I'm {FullName} and my speciality is {Specialty}");
+        }
+    }
+
+    public class Jordan : ITeammate
+    {
+        public string Specialty { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string FullName {
+            get { return FirstName + " " + LastName;}
+        }
+
+        // Constructor
+        public Jordan (string spec, string first, string last)
+        {
+            this.Specialty = spec;
+            this.FirstName = first;
+            this.LastName = last;
+        }
+        public void Work()
+        {
+            Console.WriteLine($"la di da, I'm {FullName} and my speciality is {Specialty}");
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<ITeammate> serverSide = new List<ITeammate>(){
+                new Matt ("sweaters", "Matt", "Augsberger"),
+                new Eliza ("great convo", "Eliza", "Something"),
+                new Andrew ("cheap beer", "Andrew", "Rock")
+            };
+
+            foreach (ITeammate person in serverSide) {
+                person.Work();
+            }
+
+            List<ITeammate> clientSide = new List<ITeammate>(){
+                new Jordan ("learning", "Jordan", "D"),
+                new Chaz ("sharing info", "Chaz", "Something"),
+                new Tamela ("making jokes", "Tamela", "Something")
+            };
+
+            foreach (ITeammate person in clientSide) {
+                person.Work();
+            }
+             
         }
     }
 }
