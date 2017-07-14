f2d1b1d8f260d7b59f73fd4e0ea8d2b5a7e7fe01
Jordan Dhaenens <jordan.dhaenens@gmail.com>
2017-07-14 14:05:11 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace dreamTeam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+// Specialty property - This holds the technology that the person enjoys the most.
+// FirstName property
+// LastName property
+// FullName property - This property is a readonly property that returns the first and last name concatenated.
+// Work() method - This will write a comical message to the console that describes the work they will do on a group project, based on their speciality.
